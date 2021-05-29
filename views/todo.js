8b2e5af63671824ff6522c89933b0236fdaf141b
Ilma Zahil <mzahil79@gamil.com>
2021-05-29 12:32:03 +0530

    updated

--- views/todo.js
+++ views/todo.js
@@ -4,6 +4,8 @@
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <link href="/asstes/styles.css" rel="stylesheet" type="text/css" />
   </head>
-<body>
-</body>
+  <body>
+    <div>
+    </div>
+  </body>
 </html>
