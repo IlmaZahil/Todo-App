c23402f7a3d66b7e84e032871a2b7514eeb2ec44
Ilma Zahil <66563618+IlmaZahil@users.noreply.github.com>
2021-05-29 12:37:53 +0530

    Updated

--- views/todo.js
+++ views/todo.js
@@ -5,7 +5,16 @@
     <link href="/asstes/styles.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    <div>
+    <div id="todo-table">
+      <form>
+        <input type= "text" name= "item" placeholder ="Add new item..." required />
+          <button type="submit">Add Item</button>
+      </form>
+      <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+      </ul>
     </div>
   </body>
 </html>
